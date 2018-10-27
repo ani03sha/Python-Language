ae997253ae66dadeb6660774fe1eb72663c9ec3b
Anirudh Sharma <anirudh03sharma@gmail.com>
2018-10-28 00:10:07 +0530

    Adding few problems in String subsection

---
+++ 03_strings/01_swap_case.py
@@ -0,0 +1,15 @@
+# You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase
+# letters and vice versa.
+#
+# For Example:
+# Www.HackerRank.com → wWW.hACKERrANK.COM
+
+
+def swap_case(s):
+    return s.swapcase()
+
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
---
+++ 03_strings/02_split_and_join.py
@@ -0,0 +1,27 @@
+# In Python, a string can be split on a delimiter.
+#
+# Example:
+#
+# >>> a = "this is a string"
+# >>> a = a.split(" ") # a is converted to a list of strings.
+# >>> print a
+# ['this', 'is', 'a', 'string']
+# Joining a string is simple:
+#
+# >>> a = "-".join(a)
+# >>> print a
+# this-is-a-string
+# Task
+# You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+
+
+def split_and_join(l):
+    # write your code here
+    l = l.split(" ")
+    return "-".join(l)
+
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
