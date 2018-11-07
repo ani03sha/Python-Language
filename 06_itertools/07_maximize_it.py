29804f07979ebf5dd50e1da812159a79bc69e984
Anirudh Sharma <anirudh03sharma@gmail.com>
2018-11-07 18:38:21 +0530

    Added problem description

--- 06_itertools/07_maximize_it.py
+++ 06_itertools/07_maximize_it.py
@@ -1,3 +1,18 @@
+# You are given a function f(X)=X^2. You are also given K lists. The ith list consists of Ni elements.
+#
+# You have to pick one element from each list so that the value from the equation below is maximized:
+#
+# S = (f(X1) + f(X2)+....f(Xk))%M
+#
+# Xi denotes the element picked from the ith list . Find the maximized value Smax obtained.
+#
+# % denotes the modulo operator.
+#
+# Note that you need to take exactly one element from each list, not necessarily the largest element. You add the
+# squares of the chosen elements and perform the modulo operation. The maximum value that you can obtain, will be the
+# answer to the problem.
+
+
 from itertools import product
 
 K, M = map(int, input().split())
